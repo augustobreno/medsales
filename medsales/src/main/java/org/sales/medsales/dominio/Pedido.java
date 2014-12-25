34a6ed1691d8a1eee383177d0b7905589e71be27
Augusto Breno <augusto.breno@trt6.jus.br>
2014-12-25 11:16:47 -0300

    Renomeando package raiz para "org.sales". Implementando regra de unicidade.

---
+++ medsales/src/main/java/org/sales/medsales/dominio/Pedido.java
@@ -0,0 +1,39 @@
+package org.sales.medsales.dominio;
+
+import java.util.List;
+
+/**
+ * Representa um pedido de medicamentos.
+ * @author Augusto
+ *
+ */
+@SuppressWarnings("serial")
+public class Pedido extends EntityBase<Long>{
+
+	/**
+	 * Todos os itens que compõem este pedido
+	 */
+	private List<Item> itens;
+	
+	/**
+	 * Desconto aplicado a todos os items do pedido.
+	 */
+	private Double desconto;
+
+	public List<Item> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+	
+}
---
+++ medsales/src/main/java/org/sales/medsales/persistencia/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package org.sales.medsales.persistencia.repository;
+
+import org.sales.medsales.dominio.Cliente;
+
+public class ClienteRepository extends CrudRepositoryBase<Cliente, Long> {
+
+}
